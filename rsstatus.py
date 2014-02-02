aeee22cd29056db947e6a1b92bea1e4c45114806
battlesysadmin <battlesysadmin@sonic.net>
2014-02-02 14:41:25 -0800

    mongodb tool for checking replica set status

---
+++ rsstatus.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# check mongodb replica sets
+# alexis_g * feb 2014
+
+import pymongo
+from pymongo import MongoClient
+import datetime
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-s", "--srchost", dest="srchost",
+ help="Source Mongo To Check", metavar="srchost")
+(options, args) = parser.parse_args()
+
+print options.srchost
+
+uri = 'mongodb://'+options.srchost+'/admin'
+client = MongoClient(uri)
+db = client["admin"]
+
+rstatus = db.command("replSetGetStatus")
+
+ for rs in rstatus["members"]:
+ try:
+ rs["optimeDate"]
+ except:
+ continue
+ else:
+ print "%s %s" % ( rs["name"].split('.')[0], datetime.datetime.utcnow() - rs["optimeDate"] )
